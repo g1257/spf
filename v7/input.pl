3c088a9b2b49b44c60ac7187a1f3d57a8cd1ed58
g1257 <gz1@ornl.gov>
2010-02-16 15:39:31 -0500

    working on input for 2orb

--- v7/input.pl
+++ v7/input.pl
@@ -1,6 +1,53 @@
 #!/usr/bin/perl -w
 
 use strict;
+
+my ($choice)=@ARGV;
+my $model="UNDEFINED";
+
+if ($choice==0)  {
+	$model = "PhononsTwoOrbitals";
+	createInput0();
+} else {
+	$model = "PnictidesTwoOrbitals";
+	createInput1();
+}
+
+sub createInput0
+{
+my $l = 8;
+my $n = $l*$l;
+my $V = 0.0;
+my $potentialV  = "";
+for (my $i=0;$i<$n;$i++) {
+	$potentialV = $potentialV." $V ";
+}
+print <<EOF;
+OPTIONS none
+VERSION SPFv7
+FILENAME output
+CARRIERS 128
+MU 0
+BETA 100
+MCTHERMALIZATIONS 0
+MCEFFECTIVE 2
+MCUNMEASURED 1
+MCWINDOW   0.5
+MCSTARTTYPE none 
+MCSTARTLEVEL 0
+HISTOGRAMSTEPS 1000
+BOUNDARYCONDITIONS periodic
+LINSIZE $l
+hoppings 
+16
+
+#EOF
+EOF	
+}
+
+
+sub createInput1
+{
 my $l = 8;
 my $n = $l*$l;
 my $V = 0.0;
@@ -40,5 +87,5 @@ JAFNN 0.0
 JAFNNN 0.0
 #EOF
 EOF
-
+}
 
