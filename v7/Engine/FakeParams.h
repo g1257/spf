498a013df36489710f6f2b012679fc434cce69aa
g1257 <gz1@ornl.gov>
2014-01-23 14:47:09 -0500

    FakeParams added

---
+++ v7/Engine/FakeParams.h
@@ -0,0 +1,31 @@
+
+/** \ingroup SPF */
+/*@{*/
+
+/*! \file FakeParams.h
+ *
+ *
+ *
+ */
+#ifndef FAKE_PARAMS_H
+#define FAKE_PARAMS_H
+#include "String.h"
+
+namespace Spf {
+
+struct FakeParams {
+
+	FakeParams(PsimagLite::String dynvarsfile1,
+	           int long long randomSeed1,
+	           PsimagLite::String options1)
+	    : dynvarsfile(dynvarsfile1),randomSeed(randomSeed1),options(options1)
+	{}
+
+	PsimagLite::String dynvarsfile;
+	int long long randomSeed;
+	PsimagLite::String options;
+}; // FakeParams
+} // namespace Spf
+
+/*@}*/
+#endif // FAKE_PARAMS_H
