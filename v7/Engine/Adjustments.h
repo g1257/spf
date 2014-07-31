eb284f8eb1d384a87a11647a14ebd6fbd558b9b0
g1257 <gz1@ornl.gov>
2014-07-31 14:44:30 -0400

    indentation

--- v7/Engine/Adjustments.h
+++ v7/Engine/Adjustments.h
@@ -18,50 +18,50 @@
 #include "RootFindingBisection.h"
 
 namespace Spf {
-	template<typename EngineParamsType>
-	class Adjustments {
-		typedef typename EngineParamsType::RealType RealType;
-	public:
-		Adjustments(const EngineParamsType& engineParams)
-		: engineParams_(engineParams)
-		{}
+template<typename EngineParamsType>
+class Adjustments {
+	typedef typename EngineParamsType::RealType RealType;
+public:
+	Adjustments(const EngineParamsType& engineParams)
+	    : engineParams_(engineParams)
+	{}
 
-		RealType adjChemPot(const typename PsimagLite::Vector<RealType>::Type& eigs) const
-		{
-			typedef DensityFunctionDiag<EngineParamsType> DensityFunctionType;
-			DensityFunctionType densityFunction(engineParams_,eigs);
-			typedef PsimagLite::RootFindingBisection<DensityFunctionType> RootFindingType;
-			// typedef RootFindingNewton<DensityFunctionType> RootFindingType;
-			RealType tolerance = engineParams_.adjustTolerance;
-			SizeType maxIter = engineParams_.adjustMaxIter;
-			RootFindingType  rootFinding(densityFunction,-10,10,maxIter,tolerance);
+	RealType adjChemPot(const typename PsimagLite::Vector<RealType>::Type& eigs) const
+	{
+		typedef DensityFunctionDiag<EngineParamsType> DensityFunctionType;
+		DensityFunctionType densityFunction(engineParams_,eigs);
+		typedef PsimagLite::RootFindingBisection<DensityFunctionType> RootFindingType;
+		// typedef RootFindingNewton<DensityFunctionType> RootFindingType;
+		RealType tolerance = engineParams_.adjustTolerance;
+		SizeType maxIter = engineParams_.adjustMaxIter;
+		RootFindingType  rootFinding(densityFunction,-10,10,maxIter,tolerance);
 
-			RealType mu = rootFinding();
-			//std::cerr<<" new mu = "<<mu<<"\n";
-			return mu;
-		}
+		RealType mu = rootFinding();
+		//std::cerr<<" new mu = "<<mu<<"\n";
+		return mu;
+	}
 
-		template<typename SomeTpemType>
-		RealType adjChemPot(const typename PsimagLite::Vector<RealType>::Type& moments,
-		                    const SomeTpemType& tpem) const
-		{
-			typedef DensityFunctionTpem<EngineParamsType,SomeTpemType> DensityFunctionType;
-			DensityFunctionType densityFunction(engineParams_,moments,tpem);
-			typedef PsimagLite::RootFindingBisection<DensityFunctionType> RootFindingType;
-			// typedef RootFindingNewton<DensityFunctionType> RootFindingType;
-			RealType tolerance = engineParams_.adjustTolerance;
-			SizeType maxIter = engineParams_.adjustMaxIter;
-			RootFindingType  rootFinding(densityFunction,-10,10,maxIter,tolerance);
+	template<typename SomeTpemType>
+	RealType adjChemPot(const typename PsimagLite::Vector<RealType>::Type& moments,
+	                    const SomeTpemType& tpem) const
+	{
+		typedef DensityFunctionTpem<EngineParamsType,SomeTpemType> DensityFunctionType;
+		DensityFunctionType densityFunction(engineParams_,moments,tpem);
+		typedef PsimagLite::RootFindingBisection<DensityFunctionType> RootFindingType;
+		// typedef RootFindingNewton<DensityFunctionType> RootFindingType;
+		RealType tolerance = engineParams_.adjustTolerance;
+		SizeType maxIter = engineParams_.adjustMaxIter;
+		RootFindingType  rootFinding(densityFunction,-10,10,maxIter,tolerance);
 
-			RealType mu = rootFinding();
-			//std::cerr<<" new mu = "<<mu<<"\n";
-			return mu;
-		}
+		RealType mu = rootFinding();
+		//std::cerr<<" new mu = "<<mu<<"\n";
+		return mu;
+	}
 
-	private:
+private:
 
-		const EngineParamsType& engineParams_;
-	}; // Adjustments
+	const EngineParamsType& engineParams_;
+}; // Adjustments
 } // namespace Spf
 
 /*@}*/
