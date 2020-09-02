51a3b5083c01f7ef400de0bf2eab3893743d851d
Gonzalo Alvarez <gz1@ornl.gov>
2020-09-02 00:23:41 -0400

    configure.pl fixed

--- v7/configure.pl
+++ v7/configure.pl
@@ -1,17 +1,35 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+=pod
+Copyright (c) 2006-2020, UT-Battelle, LLC
+All rights reserved
+
+[SPF, Version 7.]
+
+*********************************************************
+THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
+CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED.
+
+Please see full open source license included in file LICENSE.
+*********************************************************
+
+=cut
+use warnings;
+use strict;
+
 use Getopt::Long qw(:config no_ignore_case);
 use lib "../../PsimagLite/scripts";
 use NewMake;
 use lib ".";
 use PsiTag;
 
-my ($flavor, $generateSources, $su2enabled, $lto) = (NewMake::noFlavor() , 0, 0, 0);
-my $usage = "USAGE: $0 [-f flavor] [-s] [-su2] [-lto] [-c config]\n";
+my ($flavor, $lto) = (NewMake::noFlavor() , 0);
+my $usage = "USAGE: $0 [-f flavor] [-lto] [-c config]\n";
 my $config;
 
 GetOptions('f=s' => \$flavor,
-           's' => \$generateSources,
-           'su2' => \$su2enabled,
            'lto' => \$lto,
            'c=s' => \$config) or die "$usage\n";
 
@@ -23,20 +41,34 @@ if ($lto == 1) {
 	$lto = "";
 }
 
-my @configFiles = ("../TestSuite/inputs/ConfigBase.psiTag");
+my @configFiles = ("TestSuite/inputs/ConfigBase.psiTag");
 push @configFiles, $config if (defined($config));
 
-sub writeMakefile
-{
-	open(my $fh,">Makefile") or die "Cannot open Makefile for writing: $!\n";
+my %spf = (name => "spf", dotos => "spf.o");
+#my %progGlob = (name => "ProgramGlobals", aux => 1);
 
-	my $libs = "$lapack  $glsL  -lm  -lpthread -lpsimaglite";
-	my $cxx = "g++";
-	my $cppflags = " -O3 -DNDEBUG  -I../Tpem -IEngine -I../../PsimagLite ";
-	$cppflags .= " -I../../PsimagLite/src  $gslC";
-	Make::make($fh,\@drivers,"spf","Linux",0,$libs,$cxx,$cppflags,"true"," "," ");
+my @drivers = (\%spf);
+
+my %args;
+$args{"CPPFLAGS"} = $lto;
+$args{"LDFLAGS"} = $lto;
+$args{"flavor"} = $flavor;
+$args{"code"} = "SPFv7";
+$args{"configFiles"} = \@configFiles;
+
+createMakefile(\@drivers, \%args);
+
+sub createMakefile
+{
+	my ($drivers, $args) = @_;
+	NewMake::backupMakefile();
+
+	my $fh;
+	open($fh, ">", "Makefile") or die "Cannot open Makefile for writing: $!\n";
+
+	NewMake::main($fh, $args, \@drivers);
 
 	close($fh);
-	print "$0: Done writing Makefile\n";
+	print STDERR "$0: File Makefile has been written\n";
 }
 
